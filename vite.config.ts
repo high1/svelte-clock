677ac8b78b8c237a7bb9510a7f206502b10ef2a4
high1 <high1@mailfence.com>
2023-11-28 22:38:08 +0100

    Added tests

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,36 @@ import { defineConfig, loadEnv, type ConfigEnv } from 'vite';
 import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import uno from 'unocss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { checker } from 'vite-plugin-checker';
 
 export default ({ mode }: ConfigEnv) =>
   defineConfig({
     base: loadEnv(mode, process.cwd(), '')['BASE'] ?? '',
+    build: {
+      cssMinify: 'lightningcss',
+    },
+    css: {
+      transformer: 'lightningcss',
+    },
     plugins: [
-      tsconfigPaths({
-        projects: ['tsconfig.json', 'tsconfig.svelte.json'],
-        loose: true,
-      }),
-      svelte({ preprocess: vitePreprocess() }),
+      tsconfigPaths({ loose: true }),
       uno(),
+      svelte({
+        hot: mode === 'development',
+        preprocess: vitePreprocess({}),
+      }),
+      checker({
+        typescript: true,
+        eslint: {
+          lintCommand: 'eslint . --max-warnings 0',
+        },
+      }),
     ],
+    test: {
+      coverage: {
+        include: ['src'],
+      },
+      environment: 'happy-dom',
+      setupFiles: 'test.config.ts',
+    },
   });
