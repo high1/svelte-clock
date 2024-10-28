f998eb0455fa8b3535134a45cee83a38fec0bbbe
high1 <high1@users.noreply.github.com>
2024-10-28 10:26:22 +0100

    Updated to svelte 5, and everything else along the way (#173)
    * Updated to svelte 5, and everything else along the way
    
    * Disable concurrently for the time being

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
+/// <reference types="@testing-library/jest-dom" />
 
 interface ImportMetaEnv {
   readonly BASE?: string;
