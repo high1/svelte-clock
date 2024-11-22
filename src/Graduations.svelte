4773fc96edf64b5a633f7d585954b014f19bbdb4
high1 <high1@mailfence.com>
2024-11-22 23:18:16 +0100

    Refactored the solution
    - moved time related calculations in a separate file
    - moved common variables into a separate file also
    - separated the ClockFace, ClockHands and Graduations, and created a
    ClockLine component which serves as a basis for all lines on the
    ClockFace
    - added prettier-plugin-tailwindcss

---
+++ src/Graduations.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import { getTestId } from 'utilities';
+  import { base, rotate } from 'common';
+  import Graduation from 'ClockLine.svelte';
+</script>
+
+{#each { length: base } as _, index}
+  {@const isHour = index % 5 === 0}
+  <Graduation
+    transform={rotate(index / base, 0)}
+    class={isHour
+      ? 'stroke-zinc-600 stroke-2 dark:stroke-zinc-200'
+      : 'stroke-zinc-200 dark:stroke-zinc-600'}
+    length={isHour ? 6 : 2.5}
+    graduation
+    data-testid={getTestId(`clock-graduation-${index}`)}
+  />
+{/each}
---
+++ src/common.ts
@@ -0,0 +1,4 @@
+export const base = 60;
+
+export const rotate = (rotate: number, fractionDigits = 1) =>
+  `rotate(${(rotate * 360).toFixed(fractionDigits)})`;
