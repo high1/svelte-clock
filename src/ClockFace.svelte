23727c99e7b71e4be6c4e2e31010025dbe6a91fc
high1 <high1@users.noreply.github.com>
2025-01-06 18:28:51 +0100

    Added svelte icon to the clock itself (#231)
    - added svelte icon to the clock in the center, because it looks cool
    - used viewbox for the SVG so it's now possible to render it without
    it being skewed, which it was before
    - removed color customizations, there's little benefit to it

--- src/ClockFace.svelte
+++ src/ClockFace.svelte
@@ -9,6 +9,7 @@
   data-testid={getTestId('clock-face')}
 >
   <svg viewBox="0 0 200 200" class="h-[95vmin]">
+    <image class="size-1/6 translate-5/12" href="logo.svg" />
     <g class="translate-1/2">
       <circle class="fill-none stroke-zinc-600 dark:stroke-zinc-200" r="98" />
       <Graduations />
