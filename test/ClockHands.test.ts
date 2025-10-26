20e5f6ba4b5a2242f8b0378a0279bfd28d41f8e4
high1 <high1@users.noreply.github.com>
2025-10-26 21:53:08 +0100

    feat(test): switch to vitest browser mode (#342)
    * feat(test): switch to vitest browser mode
    
    * fix(test): Install playwright
    
    * fix(test): Install only headless chromium

--- test/ClockHands.test.ts
+++ test/ClockHands.test.ts
@@ -1,65 +1,63 @@
-import { afterAll, beforeAll, describe, expect, test, vi } from 'vitest';
-import { render, screen } from '@testing-library/svelte';
+import { beforeAll, describe, expect, test, vi } from 'vitest';
+import { render } from 'vitest-browser-svelte';
 import { time } from 'time.svelte';
 import ClockHands from 'ClockHands.svelte';
-import { beforeEach } from 'node:test';
 
 describe('<ClockHands />', () => {
   beforeAll(() => {
-    vi.stubEnv('TZ', 'UTC');
     vi.hoisted(() => {
-      vi.setSystemTime(new Date().setHours(0, 0, 0, 0));
+      vi.setSystemTime(new Date(2020, 0).setHours(0, 0, 0, 0));
     });
   });
-  beforeEach(() => {
-    vi.setSystemTime(new Date().setHours(0, 0, 0, 0));
-  });
-  afterAll(() => {
-    vi.unstubAllEnvs();
-  });
+  // beforeEach(() => {
+  //   vi.setSystemTime(new Date().setHours(0, 0, 0, 0));
+  // });
+  // afterEach(() => {
+  //   vi.unstubAllEnvs();
+  // });
   test('renders starting hours correctly', () => {
-    render(ClockHands);
-    expect(screen.getByTestId('clock-hour').getAttribute('transform')).toBe(
-      'rotate(0.0)',
-    );
+    const screen = render(ClockHands);
+    expect(
+      screen.getByTestId('clock-hour').element().getAttribute('transform'),
+    ).toBe('rotate(0.0)');
   });
   test('renders elapsed hours correctly', () => {
     const elapsedHours = Math.floor(Math.random() * 12);
-    vi.setSystemTime(new Date().setHours(elapsedHours, 0, 0, 0));
+    vi.setSystemTime(new Date(2020, 0).setHours(elapsedHours, 0, 0, 0));
     time.update();
-    render(ClockHands);
-    expect(screen.getByTestId('clock-hour').getAttribute('transform')).toBe(
-      `rotate(${elapsedHours * 30}.0)`,
-    );
+    const screen = render(ClockHands);
+    expect(
+      screen.getByTestId('clock-hour').element().getAttribute('transform'),
+    ).toBe(`rotate(${elapsedHours * 30}.0)`);
   });
   test('renders starting minutes correctly', () => {
-    render(ClockHands);
-    expect(screen.getByTestId('clock-minute').getAttribute('transform')).toBe(
-      'rotate(0.0)',
-    );
+    const screen = render(ClockHands);
+    expect(
+      screen.getByTestId('clock-minute').element().getAttribute('transform'),
+    ).toBe('rotate(0.0)');
   });
   test('renders elapsed minutes correctly', () => {
     const elapsedMinutes = Math.floor(Math.random() * 59);
-    vi.setSystemTime(new Date().setHours(0, elapsedMinutes, 0, 0));
+    vi.setSystemTime(new Date(2020, 0).setHours(0, elapsedMinutes, 0, 0));
     time.update();
-    render(ClockHands);
-    expect(screen.getByTestId('clock-minute').getAttribute('transform')).toBe(
-      `rotate(${elapsedMinutes * 6}.0)`,
-    );
+    const screen = render(ClockHands);
+    expect(
+      screen.getByTestId('clock-minute').element().getAttribute('transform'),
+    ).toBe(`rotate(${elapsedMinutes * 6}.0)`);
   });
   test('renders starting seconds correctly', () => {
-    render(ClockHands);
-    expect(screen.getByTestId('clock-second').getAttribute('transform')).toBe(
-      'rotate(0.0)',
-    );
+    const screen = render(ClockHands);
+    expect(
+      screen.getByTestId('clock-second').element().getAttribute('transform'),
+    ).toBe('rotate(0.0)');
   });
   test('renders elapsed seconds correctly', () => {
     const elapsedSeconds = Math.floor(Math.random() * 59);
-    vi.setSystemTime(new Date().setHours(0, 0, elapsedSeconds, 0));
+    vi.setSystemTime(new Date(2020, 0).setHours(0, 0, elapsedSeconds, 0));
     time.update();
-    render(ClockHands);
-    expect(screen.getByTestId('clock-second').getAttribute('transform')).toBe(
-      `rotate(${elapsedSeconds * 6}.0)`,
-    );
+    const screen = render(ClockHands);
+    expect(
+      screen.getByTestId('clock-second').element().getAttribute('transform'),
+    ).toBe(`rotate(${elapsedSeconds * 6}.0)`);
   });
 });
