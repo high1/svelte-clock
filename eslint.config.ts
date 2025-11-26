9e933f2ed7f5bce6f0f7d03f13caf6cebfa96ec7
high1 <high1@users.noreply.github.com>
2025-11-26 10:20:03 +0100

    chore(lint): Removed html-eslint due to config issues (#358)

--- eslint.config.ts
+++ eslint.config.ts
@@ -13,7 +13,6 @@ import jsonc from 'eslint-plugin-jsonc';
 import yml from 'eslint-plugin-yml';
 import { importX } from 'eslint-plugin-import-x';
 import stylistic from '@stylistic/eslint-plugin';
-import html from '@html-eslint/eslint-plugin';
 import { fileURLToPath } from 'node:url';
 import { createTypeScriptImportResolver } from 'eslint-import-resolver-typescript';
 
@@ -71,26 +70,12 @@ export default defineConfig(
       },
     },
   },
-  {
-    files: ['**/*.html'],
-    ...html.configs['flat/recommended'],
-    rules: {
-      ...html.configs['flat/recommended'].rules,
-      '@html-eslint/indent': ['error', 2],
-      '@html-eslint/require-closing-tags': ['error', { selfClosing: 'always' }],
-      '@html-eslint/no-extra-spacing-attrs': [
-        'error',
-        { enforceBeforeSelfClose: true },
-      ],
-      '@html-eslint/attrs-newline': 'off',
-    },
-  },
   jsonc.configs['flat/recommended-with-jsonc'],
   jsonc.configs['flat/prettier'],
   yml.configs['flat/recommended'],
   yml.configs['flat/prettier'],
   {
-    files: ['**/*.{html,json,yml,yaml}'],
+    files: ['**/*.{json,yml,yaml}'],
     ...tseslintConfigs.disableTypeChecked,
   },
 );
