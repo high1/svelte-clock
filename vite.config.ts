2612f4dc4772887612c93c7757c4fe72739bb574
high1 <high1@mailfence.com>
2026-01-15 11:51:59 +0100

    chore(deps): Removed vite-tsconfig-paths

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { playwright } from '@vitest/browser-playwright';
 import { defineConfig, loadEnv } from 'vite';
 import { checker } from 'vite-plugin-checker';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => ({
   base: loadEnv(mode, process.cwd(), '')['BASE'] ?? '',
   plugins: [
-    ...(mode === 'test'
-      ? [tsconfigPaths({ loose: true, projectDiscovery: 'lazy' })]
-      : []),
     tailwindcss(),
     svelte({
       compilerOptions: {
@@ -32,6 +28,9 @@ export default defineConfig(({ mode }) => ({
     tsconfigPaths: true,
   },
   test: {
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname,
+    },
     browser: {
       enabled: true,
       headless: true,
