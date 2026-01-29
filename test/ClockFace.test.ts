4eb3f4a53a74548888dd2afbf31a229452a1b695
high1 <high1@mailfence.com>
2026-01-30 00:42:19 +0100

    fix(test): Separate config, cleanup

--- test/ClockFace.test.ts
+++ test/ClockFace.test.ts
@@ -2,10 +2,16 @@ import { describe, expect, test } from 'vitest';
 import { render } from 'vitest-browser-svelte';
 
 import ClockFace from '@/ClockFace.svelte';
+import { clockFaceId } from '@/common';
 
 describe('<ClockFace />', () => {
   test('renders clock face', () => {
     const screen = render(ClockFace);
-    expect(screen.getByTestId('clock-face')).toBeInTheDocument();
+    expect(screen.getByTestId(clockFaceId)).toBeInTheDocument();
+  });
+  test('unmounts clock face', () => {
+    const screen = render(ClockFace);
+    screen.unmount();
+    expect(screen.getByTestId(clockFaceId)).not.toBeInTheDocument();
   });
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
-/// <reference types="vitest/config" />
-
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
-import { playwright } from '@vitest/browser-playwright';
 import { defineConfig, loadEnv } from 'vite';
 import { checker } from 'vite-plugin-checker';
 
@@ -25,18 +22,5 @@ export default defineConfig(({ mode }) => ({
   ],
   resolve: {
     tsconfigPaths: true,
-  },
-  test: {
-    alias: {
-      '@/': new URL('./src/', import.meta.url).pathname,
-    },
-    browser: {
-      enabled: true,
-      headless: true,
-      instances: [{ browser: 'chromium' }],
-      provider: playwright(),
-      screenshotFailures: false,
-    },
-    environment: 'happy-dom',
   },
 }));
