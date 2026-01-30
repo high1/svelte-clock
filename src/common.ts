c9c38ea1836993f2444c6c5e1e29dcd576c5558b
high1 <high1@mailfence.com>
2026-01-30 14:45:19 +0100

    fix(test): Moved reused test ids into common

--- src/common.ts
+++ src/common.ts
@@ -2,3 +2,7 @@ export const seconds = 60;
 export const rotate = (rotate: number, fractionDigits = 1) =>
   `rotate(${(rotate * 360).toFixed(fractionDigits)})`;
 export const clockFaceId = 'clock-face';
+export const clockGraduationId = 'clock-graduation';
+export const clockHourId = 'clock-hour';
+export const clockMinutedId = 'clock-minute';
+export const clockSecondId = 'clock-second';
